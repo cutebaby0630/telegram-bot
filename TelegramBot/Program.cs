1ee23325040eb44a4e7cb0ca0a49fe40443c7beb
cutebaby0630 <lovemath0630@gmail.com>
2020-07-13 16:56:57 +0800

    send message to telegram from console

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static Telegram.Bot.TelegramBotClient Bot = new Telegram.Bot.TelegramBotClient("1360100028:AAEbfPyPmHXEqXoFJYnah2ek9Hqhfwq_Tf4");
+        static long chanelId = -1001289096681;
+        private void Tc_MessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Bot.SendTextMessageAsync(e.Message.Chat.Id, "你剛才說:" + e.Message.Text);
+            //MessageBox.Show(e.Message.Text);
+        }
+            
+        static void Main(string[] args)
+        {
+   
+            // 透過SendTextMessageAsync來傳送訊息，傳入chanelid及要傳送的訊息
+            Bot.SendTextMessageAsync(chanelId, "每5秒傳送一個訊息");
+
+            Bot.SendTextMessageAsync(chanelId, "Mickey Mouse");
+
+            Console.ReadLine();
+        }
+    }
+}
