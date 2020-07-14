951d56ba8d7a92a990c2e6749d4bd70663951e91
cutebaby0630 <lovemath0630@gmail.com>
2020-07-14 10:45:42 +0800

    Echo message

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,26 +1,67 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot
 {
+
     class Program
     {
-        static Telegram.Bot.TelegramBotClient Bot = new Telegram.Bot.TelegramBotClient("1360100028:AAEbfPyPmHXEqXoFJYnah2ek9Hqhfwq_Tf4");
+        private static readonly Telegram.Bot.TelegramBotClient Bot = new Telegram.Bot.TelegramBotClient("1360100028:AAEbfPyPmHXEqXoFJYnah2ek9Hqhfwq_Tf4");
         static long chanelId = -1001289096681;
-        private void Tc_MessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
-        {
-            Bot.SendTextMessageAsync(e.Message.Chat.Id, "你剛才說:" + e.Message.Text);
-            //MessageBox.Show(e.Message.Text);
-        }
-            
         static void Main(string[] args)
         {
-   
             // 透過SendTextMessageAsync來傳送訊息，傳入chanelid及要傳送的訊息
+            //chanelId test
             Bot.SendTextMessageAsync(chanelId, "每5秒傳送一個訊息");
-
             Bot.SendTextMessageAsync(chanelId, "Mickey Mouse");
 
+            var me = Bot.GetMeAsync().Result;
+            Console.Title = me.Username;
+            Bot.OnMessage += BotOnMessageEcho;
+            Bot.OnMessageEdited += BotOnMessageEcho;
+           /* Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnReceiveError += BotOnReceiveError;*/
+            Bot.StartReceiving(Array.Empty<UpdateType>());
+            Console.WriteLine($"Start listening for @cutebaby0630_Bot");
             Console.ReadLine();
+            Bot.StopReceiving();
         }
+
+        private static async void BotOnMessageEcho(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message == null || message.Type != MessageType.Text) return;
+            switch (message.Text)
+            {
+                case "":
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+                        var keyboard = new InlineKeyboardMarkup(
+                        InlineKeyboardButton.WithUrl("Talk to me in private", "https://t.me/cutebaby0630_Bot"));
+                        await Bot.SendTextMessageAsync(chanelId, "你剛才說:" + message.Text);
+                        break;
+                    }
+            }
+        }
+       /* private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            var callbackQuery = callbackQueryEventArgs.CallbackQuery;
+            await Bot.SendTextMessageAsync(
+                callbackQuery.Message.Chat.Id,
+                $"Received {callbackQuery.Data}");
+        }
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Console.WriteLine("Received error: {0} — {1}",
+                receiveErrorEventArgs.ApiRequestException.ErrorCode,
+                receiveErrorEventArgs.ApiRequestException.Message);
+        }*/
     }
 }
